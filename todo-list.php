4250699e19caf7dc3f42c648295aa68e5a9f42bc
iandresabad <iandresabad@yahoo.com>
2014-02-26 09:20:27 -0600

    need the overwrite

--- todo-list.php
+++ todo-list.php
@@ -1,81 +1,68 @@
 <?php
-	
-function get_file_added($file) {
+//Read a file and then return it
+function read_file($file) {
 	$handle = fopen($file, "r");
-	$setFile = filesize($file);
-	if ($setFile > 0) {
-		$contents = fread($handle, filesize($file)); 
-		fclose($handle); 
-		return explode("\n", $contents); 
-	} else {
-		echo "You don't have items on your list"; 
-		return array(); 
-	}
-}  
+		$contents = fread($handle, filesize($file));
+	fclose($handle);
+	return explode("\n", $contents);
+}
 
+//save file txt to an array
 function save_file($file, $array) {
-    $handle = fopen($file, 'w');
-    $saveList = implode("\n", $array);
-    fwrite($handle, $saveList);
-    fclose($handle);
-
+	$handle = fopen($file, 'w');
+	$saveList = implode("\n", $array);
+	fwrite($handle, $saveList);
+	fclose($handle);
 }
 
+//open and set a file location 
 $file = "data/todo_list.txt";
-$items = get_file_added($file);
+$archiveFile = "data/archives.txt";
+$archives = read_file($archiveFile);
+//it checks if the file is not empty 
+$items = (filesize($file) > 0) ? read_file($file) : array();
 
-if (!empty($_POST)) {
+//then add the items to the list
+if (!empty($_POST['newItem'])) {
 	array_push($items, $_POST['newItem']);
-	save_file("data/todo_list.txt", $items);
-	header("Location: todo-list.php");
-}
-
-if (!empty($_GET)) {
-	array_splice($items, $_GET['remove'], 1);
 	save_file($file, $items);
-	header("Location: todo-list.php");
-} 
-
-if(isset($_POST['newItem'])) {
-	array_push($file, $items);
-	save_file($file, $items);
+	header("location: todo-list.php");
+	exit(0);
 }
 
 if(isset($_GET['remove'])) {
-	$itemId = $_GET['remove'];
-	unset($items[$itemId]);
-	save_file($file, $items); 
-}
-
-$errorMessage = '';
-if (count($_FILES) > 0 && $_FILES['file1']['error'] != 0){
-	if($_FILES['file1']['type'] != 'text/plain') {
-		$errorMessage = 'ERROR: INVALID FILE TYPE.';
-	}elseif($_FILES['file1']['error'] != 0) {
-		$errorMessage = 'ERROR UPLOADING FILE';
-	}else {
-	$upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
-	$filename = basename($_FILES['file1']['name']);
-	$saved_filename = $upload_dir . $filename;
-	move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
-
-	if ($_POST['file0'] == TRUE) {
-		$items = $fileContents;
-	}else {
-		$items = array_merge($items, $fileContents); 
-	}
-	
+	$archiveItem = array_splice($items, $_GET['remove'], 1);
+	$archives = array_merge($archives, $archiveItem);
+	save_file($archiveFile, $archives);
 	save_file($file, $items);
 	header("Location: todo-list.php");
 	exit(0);
+}
+
+$errorMessage = '';
+//errros that are given if their is no text file when file uploaded
+if(count($_FILES) > 0) {
+	if($_FILES['file1']['error'] != 0) {
+		$errorMessage = 'ERROR UPLOADING FILE.';
+	}elseif ($_FILES['file1']['type'] != 'text/plain') {
+		$errorMessage = 'ERROR: INVALID FILE TYPE.';
+	}else {
+		$upload_dir = '/vagrant/sites/codeup.dev/public/uploads';
+		$filename = basename($_FILES['file1']['name']);
+		$saved_filename = $upload_dir . $filename;
+		move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+		$fileContents = read_file($saved_filename);
+
+		if ($_POST['file0'] == TRUE) {
+			$items = $fileContents;
+		}else {
+			$items = array_merge($items, $fileContents);
+		}
+
+		save_file($file, $items);
+		header("Location: todo-list.php");
+		exit(0);
 	}
-} 
-
-
-if (isset($saved_filename)) {
-
-	echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
-
 }
 
 ?>
@@ -83,33 +70,36 @@ if (isset($saved_filename)) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>File Upload</title>
+	<title>TODO List</title>
 </head>
 <body>
-	<h2>TODOs!</h2>
-	<ul>
-		<? foreach ($items as $key => $item): ?>
-			<li><?= htmlspecialchars(strip_tags($item)); ?> <a href="?remove=<?php echo $key; ?>"> Delete</a></li>
-		<? endforeach; ?>
-	</ul>
-	<h3>Add a TODO list item:</h3>
-	<form method="POST" action="todo-list.php">
-	<p>
-		<label for="newItem">New Item:</label>
-			<input id="newItem" name="newItem" type="text" autofocus="autofocus"> 
-			<button type="submit">Add Item</button>
-	</p>
-	<? if(!empty($errorMessage)) : ?>
-		<a><?= $errorMessage; ?></a>
-	<? endif; ?>
-	<p>
 
-		<label for="file1"></label>
-		<input type="file" id="file1" name="file1">
-	</p>
-	<p>
-		<input type="submit" value="upload">
-	</p>
-</form>
+	<h2>TODO List</h2>
+		<ul>
+			<? foreach ($items as $key => $item): ?>
+				<li><?= htmlspecialchars(strip_tags($item)); ?> <a href="?remove=<?= $key; ?>">DELETE</a></li>
+				<? endforeach; ?>
+		</ul>
+		<h3>Add a TODO List item:</h3>
+		<form method="POST" enctype="multipart/form-data" action="todo-list.php">
+			<p>
+				<label for="newItem">New Item</label>
+				<input id="newItem" name ="newItem" type="text" autofocus="autofocus">
+			</p>
+			<p>
+				<? if (!empty($errorMessage)) : ?>
+				<?= $errorMessage; ?>
+				<? endif; ?>
+			</p>
+			<p>
+				<label for="file1">File to upload: </label>
+				<input id="file1" name="file1" type="file">
+			</p>
+			<p>
+				<label for="file0">Overwrite File? </label>
+				<input id="file0" name="file0" type="checkbox">
+			</p>
+				<button type="submit">Add Item</button>
+			</form>
 </body>
 </html>
